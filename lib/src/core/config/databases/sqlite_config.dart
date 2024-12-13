38d95d584f58218121376a257a5f7d4a4a59a77e
Cainã Rinaldi Esteche <cairinest@hotmail.com>
2024-12-12 23:33:28 -0300

    Removed id from local

--- lib/src/core/config/databases/sqlite_config.dart
+++ lib/src/core/config/databases/sqlite_config.dart
@@ -4,13 +4,12 @@ abstract class SQLiteConfig {
 
   static const List<String> onCreate = [
     """CREATE TABLE Newsletter (
-      id INTEGER PRIMARY KEY,
       title TEXT NOT NULL,
       category TEXT NOT NULL,
       summary TEXT NOT NULL,
       link TEXT NOT NULL,
       createdAt DATETIME NOT NULL,
-      uuid TEXT NOT NULL,
+      uuid TEXT NOT NULL PRIMARY KEY,
       remote TEXT
     );"""
   ];
--- lib/src/data/models/newsletter/newsletter_local.dart
+++ lib/src/data/models/newsletter/newsletter_local.dart
@@ -1,22 +1,20 @@
 import 'package:newsletter/src/data/models/newsletter/newsletter.dart';
 
 class NewsletterLocal extends NewsletterModel {
-  final int? id;
   final String? remote;
 
-  const NewsletterLocal(
-      {required super.title,
-      required super.category,
-      required super.summary,
-      required super.link,
-      required super.createdAt,
-      required super.uuid,
-      required this.remote,
-      this.id});
+  const NewsletterLocal({
+    required super.title,
+    required super.category,
+    required super.summary,
+    required super.link,
+    required super.createdAt,
+    required super.uuid,
+    required this.remote,
+  });
 
   NewsletterLocal.fromJson({required Map<String, dynamic> json})
       : remote = json['remote'],
-        id = json['id'],
         super(
             title: json['title'],
             category: json['category'],
